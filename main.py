768f171800ccf3d8c2f49b624c9da92ac8ba7b92
Melael <lugli.boris@gmail.com>
2021-02-18 22:05:37 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   Structure site.html
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from flask import Flask, url_for
+app = Flask(__name__)
 
+@app.route('/')
+def accueil():
+    return 'Page Accueil'
+
+@app.route('/recette')
+def recette():
+    return 'Page des recettes'
+
+@app.route('/ingrédients')
+def ingrédient():
+    return 'Page des ingrédients pour les courses'
